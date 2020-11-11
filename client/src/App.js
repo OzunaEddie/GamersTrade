703d42f8a4f4197b5f03ff48d5eec50c0cb1baee
Sandy Qiu <sandyqiu2017@gmail.com>
2020-11-10 19:17:36 -0500

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ import NavLogin from './components/navbars/nav_login/NavLogin';
 import Register from './components/access_pages/register/Register';
 import UserLogin from './components/access_pages/user_login/UserLogin';
 import ForgotPassword from './components/access_pages/forgot_password/ForgotPassword';
-import FootHome from "./components/navbars/footer/FootHome";
+import FootHome from "./components/footer/FootHome";
 
 class App extends React.Component {
 
