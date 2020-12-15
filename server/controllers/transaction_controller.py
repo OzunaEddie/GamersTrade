b2ffcfbcb717b4a109a8479ac0b7f366133ac5ee
kevin Alvarez <kevinalvarez1025@gmail.com>
2020-12-14 19:28:03 -0500

    Method for adding transactions (purchase)

--- server/controllers/transaction_controller.py
+++ server/controllers/transaction_controller.py
@@ -9,6 +9,19 @@ import json
 bp = Blueprint('transaction', __name__, url_prefix='/transaction')
 
 
+@bp.route('/purchase', methods=['GET', 'POST'])
+@token
+def addTransaction():
+    if request.method == 'POST':
+        transaction = transaction_model.TransactionModel()
+        req = request.json
+        listing = listing_model.ListingModel(listingId=req['listingId'])
+        transaction.addTransaction(listingId=req['listingId'],sellerId=listing.getUserId(),buyerId=session['userId'],price=listing.getPrice())
+        return json.dumps({'Added': True})
+
+    return json.dumps({'Added': False})
+
+
 @bp.route('/history', methods=['GET', 'POST'])
 @token_required
 def showTransactions():
